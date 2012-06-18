f4f40fdbeccc527d95b1b74d58679e7d984ee273
Swati Gupta <ContributeForACause@yagoo.in>
2012-06-18 12:56:47 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContributeForACause::Application.config.secret_token = '514b3dea52cb674728c6f15f967e9f892415438ca9977e1cdd87a5ff76f7e2ada7ea4f019dccdbea033c872d74a1782b8c490aa0f1c511b6aa6754ea7537dde9'
